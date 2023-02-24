b4455aa66f7e84629c3f4623c844341ecc31183a
martin.vallone <martin.vallone@fiqus.com>
2023-02-24 11:50:55 -0300

    adding data transformation scaffolding

---
+++ data_transformation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def transform_twitter_data(data):
+    return data
+
+
+def calculate_company_index(linkedin_data, twitter_data, sentiments_df):
+    # TODO we should create kind of a formula to calculate company_index
+    company_index = 0
+    return company_index
+
+
+def get_sentiments(data):
+    sentiments = pd.DataFrame()
+    return sentiments
+
+
+def get_top_pos_neg_tokens(sentiments_df):
+    top_pos_neg_tokens = pd.DataFrame()
+    return top_pos_neg_tokens
